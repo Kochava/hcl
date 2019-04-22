fe9505115539fd1dfa29a42202987befc8d536ef
Lang Martin <lang@hashicorp.com>
2019-04-22 11:22:51 -0400

    if a pointer points to data, use it rather than alloc a new one

--- decoder.go
+++ decoder.go
@@ -404,16 +404,20 @@ func (d *decoder) decodeMap(name string, node ast.Node, result reflect.Value) er
 }
 
 func (d *decoder) decodePtr(name string, node ast.Node, result reflect.Value) error {
-	// Create an element of the concrete (non pointer) type and decode
-	// into that. Then set the value of the pointer to this type.
+	// lookup the concrete (non pointer) type and decode into that
+	// if the pointer is nil, create an element of the type and set the pointer to that
 	resultType := result.Type()
-	resultElemType := resultType.Elem()
-	val := reflect.New(resultElemType)
+	val := reflect.Indirect(result)
+	if val.Kind() == reflect.Invalid || val == reflect.Zero(resultType) {
+		resultElemType := resultType.Elem()
+		val = reflect.New(resultElemType)
+		result.Set(val)
+	}
+
 	if err := d.decode(name, node, reflect.Indirect(val)); err != nil {
 		return err
 	}
 
-	result.Set(val)
 	return nil
 }
 
